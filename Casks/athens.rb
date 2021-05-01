61dc119775d46f362c2eb7ea1b6047b8fc375246
Julien ALEXANDRE <22328659+ndrpnt@users.noreply.github.com>
2021-05-01 17:25:02 +0200

    Update athens from 1.0.0-beta.76 to 1.0.0-beta.78

--- Casks/athens.rb
+++ Casks/athens.rb
@@ -1,6 +1,6 @@
 cask "athens" do
-  version "1.0.0-beta.76"
-  sha256 "8f136e804e157ee074cf055fe7587dd896aec92d4bb0351239458c341c2624c3"
+  version "1.0.0-beta.78"
+  sha256 "64a69c90d65a8042be4c0972206f77cc77f331df13743dc15a4df9f1e9ec032d"
 
   url "https://github.com/athensresearch/athens/releases/download/v#{version}/Athens-#{version}.dmg"
   appcast "https://github.com/athensresearch/athens/releases.atom"
