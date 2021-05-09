2499d186eac6396276b9cd413703d8b6756575fd
Julien ALEXANDRE <22328659+ndrpnt@users.noreply.github.com>
2021-05-09 19:31:52 +0200

    zellij 0.8.0

--- zellij.rb
+++ zellij.rb
@@ -1,9 +1,9 @@
 class Zellij < Formula
   desc "Terminal workspace with batteries included"
   homepage "https://zellij.dev/"
-  url "https://github.com/zellij-org/zellij/releases/download/v0.7.0/zellij-x86_64-apple-darwin.tar.gz"
-  version "0.7.0"
-  sha256 "81b5ee9d4a415490aa265152111ccc7f5491cfe22c2b7cdf5842f01f2e7f77e7"
+  url "https://github.com/zellij-org/zellij/releases/download/v0.8.0/zellij-x86_64-apple-darwin.tar.gz"
+  version "0.8.0"
+  sha256 "a081e80830c250667ce47740f2647e550b06f8c28220294d250d5abb07ebf29a"
   license "MIT"
 
   bottle :unneeded
